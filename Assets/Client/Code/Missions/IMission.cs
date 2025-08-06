b4e3b366aca80374b2d0dfff4c2dc85a2e9e4f09
PotokaAlex <nevskial686@gmail.com>
2025-08-06 13:10:32 +0300

    Init

---
+++ Assets/Client/Code/Missions/IMission.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client.Code.Missions
+{
+    public interface IMission
+    {
+        event Action OnStarted;
+        event Action OnMissionPointReached;
+        event Action OnFinished;
+        void Start();
+    }
+}
